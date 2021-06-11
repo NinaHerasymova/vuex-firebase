d0b114c2c892a2a6e5750ca73e573eeffcf30bf6
NinaHerasymova <predvoditeldvoryanstva@gmail.com>
2021-06-11 22:44:52 +0300

    init

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,7 +2,7 @@
     <section>
         <base-card>
             <h2>Page not found</h2>
-            <p>Something went wrong... <router-link to="/coaches">coaches</router-link> </p>
+            <p>Something went wrong...Maybe you want to find <router-link to="/coaches">coache</router-link> ?</p>
         </base-card>
     </section>
 </template>
